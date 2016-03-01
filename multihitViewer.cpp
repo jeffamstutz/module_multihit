28ca5f0998b93b0b86d4c56c6d75a281c4d3bd35
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2016-03-01 11:06:58 -0600

    fixes to work with the latest Texture2D changes in ospray

--- multihitViewer.cpp
+++ multihitViewer.cpp
@@ -441,26 +441,29 @@ namespace ospray {
       return alreadyCreatedTextures[msgTex];
 
     //TODO: We need to come up with a better way to handle different possible pixel layouts
-    OSPDataType type = OSP_VOID_PTR;
+    OSPTextureFormat type = OSP_TEXTURE_R8;
 
     if (msgTex->depth == 1) {
-      if( msgTex->channels == 3 ) type = OSP_UCHAR3;
-      if( msgTex->channels == 4 ) type = OSP_UCHAR4;
+      if( msgTex->channels == 1 ) type = OSP_TEXTURE_R8;
+      if( msgTex->channels == 3 )
+        type = msgTex->prefereLinear ? OSP_TEXTURE_RGB8 : OSP_TEXTURE_SRGB;
+      if( msgTex->channels == 4 )
+        type = msgTex->prefereLinear ? OSP_TEXTURE_RGBA8 : OSP_TEXTURE_SRGBA;
     } else if (msgTex->depth == 4) {
-      if( msgTex->channels == 3 ) type = OSP_FLOAT3;
-      if( msgTex->channels == 4 ) type = OSP_FLOAT3A;
+      if( msgTex->channels == 1 ) type = OSP_TEXTURE_R32F;
+      if( msgTex->channels == 3 ) type = OSP_TEXTURE_RGB32F;
+      if( msgTex->channels == 4 ) type = OSP_TEXTURE_RGBA32F;
     }
 
-    OSPTexture2D ospTex = ospNewTexture2D( msgTex->width,
-                                           msgTex->height,
-                                           type,
-                                           msgTex->data,
-                                           0);
+    vec2i texSize(msgTex->width, msgTex->height);
+    OSPTexture2D ospTex = ospNewTexture2D((osp::vec2i&)texSize,
+                                          type,
+                                          msgTex->data,
+                                          0);
 
     alreadyCreatedTextures[msgTex] = ospTex;
 
     ospCommit(ospTex);
-    g_tex = ospTex; // remember last texture for debugging
 
     return ospTex;
   }
