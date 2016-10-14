bbe08fd3b2c8c506989a6dc88594886d14483ed6
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2016-10-14 11:49:45 -0500

    updates for the latest OSPRay `devel`

--- multihitViewer.cpp
+++ multihitViewer.cpp
@@ -45,8 +45,8 @@ int main(int ac, const char **av)
                                   MultiHitSceneParser,
                                   DefaultLightsParser>(ac, av);
 
-  ospcommon::box3f      bbox;
-  ospray::cpp::Model    model;
+  std::deque<ospcommon::box3f>      bbox;
+  std::deque<ospray::cpp::Model>    model;
   ospray::cpp::Renderer renderer;
   ospray::cpp::Camera   camera;
 
